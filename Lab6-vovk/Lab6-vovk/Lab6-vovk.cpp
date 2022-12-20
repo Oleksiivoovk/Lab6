93f6b285a85286e436cf6e21ef0312be94925933
Oleksiivoovk <errrfe1@gmail.com>
2022-12-20 19:29:27 +0200

    ooo

---
+++ Lab6-vovk/Lab6-vovk/Lab6-vovk.cpp
@@ -0,0 +1,81 @@
+﻿
+#include <iostream>
+using namespace std;
+
+struct Date 
+{
+    int year;
+};
+    struct Tank
+{
+    int armor;
+    float calibr;
+    double speed;
+    
+    Date vipusck;
+    };
+
+
+int main()
+{
+    setlocale(LC_ALL, "Ukr");
+
+    
+    
+    Tank t34;
+    t34.armor;
+    t34.calibr;
+    t34.speed;
+    t34.vipusck.year = 1944;
+   
+    cout << "рiк випуску: " << t34.vipusck.year << endl;
+    
+    cout << "вiдповiдь 1: " << endl;
+    cin >> t34.armor;
+    
+    cout << "вiдповiдь 2: " << endl;
+    cin >> t34.calibr;
+    
+    cout << "вiдповiдь 3: " << endl;
+    cin >> t34.speed;
+   
+    cout << "правильна вiдповiдь 1: " << endl;
+    cout << 45 << endl;
+   
+    cout << "правильна вiдповiдь 2: " << endl;
+    cout << 75 << endl;
+   
+    cout << "правильна вiдповiдь 3: " << endl;
+    cout << 45 << endl;
+
+    
+    if (t34.armor != 45)
+    {
+        cout << "Ваша вiдповiдь: " << "false" << endl;
+    }
+
+    else
+    {
+        cout << "Ваша вiдповiдь: " << "true" << endl;
+    }
+    if (t34.calibr != 75)
+    {
+        cout << "Ваша вiдповiдь: " << "false" << endl;
+    }
+
+    else
+    {
+        cout << "Ваша вiдповiдь: " << "true" << endl;
+    }
+    if (t34.speed != 45)
+    {
+        cout << "Ваша вiдповiдь: " << "false" << endl;
+    }
+
+    else
+    {
+        cout << "Ваша вiдповiдь: " << "true" << endl;
+    }
+
+}
+
